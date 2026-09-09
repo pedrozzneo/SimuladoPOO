9ab899609c0c317262a991acd7016079237596ff
Pedro Neo <pedrozzneo@gmail.com>
2026-09-08 23:47:11 -0300

    feat: implement the delete method

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
     static void main() {
 
         UserAccount pedrozzneo = new UserAccount("pedrozzneo@gmail.com", "pedrozzneo");
-        pedrozzneo.publish("fjlsadf");
-        pedrozzneo.publish("fjlsadf");
-        pedrozzneo.publish("fjlsadf");
-        pedrozzneo.publish("fjlsadf");
-        System.out.println(post.show());
+        pedrozzneo.publish("1");
+        pedrozzneo.publish("2");
+        pedrozzneo.publish("3");
+        pedrozzneo.publish("4");
+        pedrozzneo.delete(2);
     }
 }
--- src/models/UserAccount.java
+++ src/models/UserAccount.java
@@ -5,6 +5,7 @@ public class UserAccount {
     private String username;
     private final UserAccount[] followers = new UserAccount[1000];
     private Post[] timeline = new Post[10];
+    private int timeLineSize = 0;
     private int timelineNextIndex = 0;
     private Post[] posts = new Post[1000];
     private int postsCount = 0;
@@ -14,11 +15,23 @@ public class UserAccount {
         this.username = username;
     }
 
+    public void delete(int postIdx){
+        if(postsCount == 0){
+            return;
+        }
+
+        for (int i = postIdx; i < postsCount - 1; i++) {
+            posts[i] = posts[i + 1];
+        }
+        postsCount--;
+    }
+
     public void updateTimeline(Post newPost){
         for (UserAccount follower : followers) {
             if(timelineNextIndex < 10){
                 follower.timeline[follower.timelineNextIndex] = newPost;
                 follower.timelineNextIndex++;
+                follower.timeLineSize++;
             }
             else{
                 follower.timeline[0] = newPost;
