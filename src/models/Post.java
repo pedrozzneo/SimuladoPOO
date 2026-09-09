842375eba14e5ae8ae16fbfd246f9171e8d80f77
Pedro Neo <pedrozzneo@gmail.com>
2026-09-08 22:28:58 -0300

    feat: implement the attributes, access modifiers and encapsulation methods like getters and setters

--- src/models/Post.java
+++ src/models/Post.java
@@ -1,5 +1,49 @@
 package models;
 
-public class Post {
+import java.time.LocalDate;
 
+public class Post {
+    private String quote;
+    private LocalDate date;
+    private int claps;
+    private int boos;
+
+    public Post(String quote) {
+        this.quote = quote;
+        date = LocalDate.now();
+        claps = 0;
+        boos = 0;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getClaps() {
+        return claps;
+    }
+
+    public int getBoos() {
+        return boos;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setClaps(int claps) {
+        this.claps = claps;
+    }
+
+    public void setBoos(int boos) {
+        this.boos = boos;
+    }
 }
--- src/models/UserAccount.java
+++ src/models/UserAccount.java
@@ -1,4 +1,27 @@
 package models;
 
 public class UserAccount {
+    private String email;
+    private String username;
+
+    public UserAccount(String email, String username) {
+        this.email = email;
+        this.username = username;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
 }
